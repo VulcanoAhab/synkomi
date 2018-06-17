46c1ffea2e05cb2441f17c3718a9755bca94780c
VulcanoAhab <vulcano.ahab@gmail.com>
2018-06-17 11:49:46 -0300

    organizing module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="synkomi",
+    version='0.1.0',
+    author="VulcanoAhab",
+    packages=["courier"],
+    url="https://github.com/VulcanoAhab/synkomi.git",
+    description="Automated Browsers",
+    install_requires=[
+        ]
+)
