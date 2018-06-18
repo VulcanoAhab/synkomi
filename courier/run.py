22a606318e71edfd036038a1b45cc69c492e2134
VulcanoAhab <vulcano.ahab@gmail.com>
2018-06-17 22:17:51 -0300

    Starting interaction design

--- courier/run.py
+++ courier/run.py
@@ -1,20 +1,31 @@
 from pySteer.api import Config
-from pyBrows import phantomSelenium, seleniumChrome
+from pyBrows import seleniumPhantom, seleniumChrome
 
-#preset Config
-Config.registerDrivers(
-    selenium_phantom=phantomSelenium,
-    selenium_chrome=seleniumChrome.Headless
-)
+if __name__ == "__main__":
+    import argparse
 
-#load Config
-Config.setFile("sample.yaml")
-CONF=Config()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--configuration", "-c")
+    args=parser.parse_args()
 
-#create browser
-browser=CONF.getDriver() #need to implement drivers args
+    #preset Config
+    Config.registerDrivers(
+        selenium_phantom_vanilla=seleniumPhantom.Vanilla,
+        selenium_phantom_chrome=seleniumPhantom.Chrome,
+        selenium_chrome=seleniumChrome.Headless
+    )
 
-#get target page
-if CONF.request["method"] != "GET":
-    raise Exception("[-] Only GET method working for now")
-browser.get(CONF.request["url"])
+    #load Config
+    Config.setFile(args.configuration)
+    CONF=Config()
+
+    #create browser
+    browser=CONF.getDriver() #need to implement drivers args
+
+    #get target page
+    if CONF.request["method"] != "GET":
+        raise Exception("[-] Only GET method working for now")
+    print("[+] GET: {}".format(CONF.request["url"]))
+    browser.get(CONF.request["url"])
+    print(browser.pageSource)
+    browser.close()
